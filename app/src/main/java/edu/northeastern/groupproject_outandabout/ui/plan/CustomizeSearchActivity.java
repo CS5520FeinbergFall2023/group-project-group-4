1bcefc33afbf77e764f3b2a1be48e6809d888164
zejacobs <jacobs.za@northeastern.edu>
2023-12-07 23:37:43 -0500

    add spinners to select search radius and min rating for custom search

--- app/src/main/java/edu/northeastern/groupproject_outandabout/ui/plan/CustomizeSearchActivity.java
+++ app/src/main/java/edu/northeastern/groupproject_outandabout/ui/plan/CustomizeSearchActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import edu.northeastern.groupproject_outandabout.ActivityType;
@@ -13,19 +15,27 @@ import edu.northeastern.groupproject_outandabout.R;
 public class CustomizeSearchActivity extends AppCompatActivity {
 
     private TextView testTV;
+    private Spinner searchRadiusSpinner;
+    private Spinner minRatingSpinner;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customize_search);
 
         testTV = findViewById(R.id.typeTestTV);
+        searchRadiusSpinner = findViewById(R.id.searchRadiusSpinner);
+        minRatingSpinner = findViewById(R.id.minRatingSpinner);
 
         // Get intent to determine the customization type
         Intent intent = getIntent();
         setLayoutByType((ActivityType)intent.getSerializableExtra("ActivityType"));
 
+        // Setup search radius and min rating spinners
+        setUpSpinners();
     }
 
+    //TODO: Make this method change the filtering options based on activity type
     private void setLayoutByType(ActivityType type) {
         Log.d("TEST", "setLayoutByType: " + type);
         switch (type) {
@@ -49,5 +59,25 @@ public class CustomizeSearchActivity extends AppCompatActivity {
                 testTV.setText("None");
 
         }
+    }
+
+    private void setUpSpinners() {
+        // Search Radius
+        ArrayAdapter<CharSequence> searchRadiusAdapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.search_Radius_Options,
+                android.R.layout.simple_spinner_item
+        );
+        searchRadiusAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        searchRadiusSpinner.setAdapter(searchRadiusAdapter);
+
+        // Minimum Rating
+        ArrayAdapter<CharSequence> minRatingAdapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.min_Rating_Options,
+                android.R.layout.simple_spinner_item
+        );
+        minRatingAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        minRatingSpinner.setAdapter(minRatingAdapter);
     }
 }
